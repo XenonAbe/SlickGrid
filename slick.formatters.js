93f8f099b83e4a68d9386b2c79809a0ec84df542
Ger Hobbelt <ger@hobbelt.com>
2014-02-27 22:21:59 +0100

    fixed crash in formatter due to it not having been updated to the new formatter/editor call interface

--- slick.formatters.js
+++ slick.formatters.js
@@ -108,7 +108,7 @@
     }
 
     //return "<span style='background:" + value  + "'>" + value + "</span>";
-    cellStyles.push("background:" + value);
+    cellMetaInfo.cellStyles.push("background:" + value);
     return "<span style='color:black; padding-left: 1px; padding-right: 1px; background-color: rgba(255, 255, 255, 0.4); text-shadow: 1px 1px 3px white; -webkit-box-shadow: 0px 0px 3px 1px rgba(255, 255, 255, 0.4); box-shadow: 0px 0px 3px 1px rgba(255, 255, 255, 0.4);'>" + value + "</span>";
   }
 
