9b63699ebbbe0378c33c62ffd853b65e31bb2b7e
Ger Hobbelt <ger@hobbelt.com>
2014-09-30 04:59:36 +0200

    slick.editors.js defined SelectCellEditor twice; merged both; example3c now correctly uses jquery-multiselect but the select list is still faulty.

--- slick.editors.js
+++ slick.editors.js
@@ -799,120 +799,6 @@ function isValidModifier(v) {
   }
 
 
-  function SelectCellEditor(args) {
-    var $select;
-    var defaultValue;
-    var scope = this;
-    var opt;
-
-    function getKeyFromKeyVal(opt, val) {
-      var i, v, index = 0;
-
-      for (i in opt) {
-        v = opt[i];
-        if (v.val == val) {
-          index = i;
-          break;
-        }
-      }
-      return index;
-    }
-
-    this.init = function() {
-        var i;
-
-        defaultValue = null;
-        opt = (args.metadataColumn && args.metadataColumn.options) || args.column.options;
-        assert(opt);
-        option_str = [];
-        for (i in opt) {
-          v = opt[i];
-          option_str.push("<OPTION value='" + v.key + "'>" + v.val + "</OPTION>");
-        }
-        $select = $('<SELECT class="editor-select">' + option_str.join('') + "</SELECT>")
-         .appendTo(args.container)
-         .focus()
-         .select();
-        // this expects the multiselect widget (http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/) to be loaded
-        $select.multiselect({
-          autoOpen: true,
-          minWidth: $(args.container).innerWidth() - 5,
-          multiple: false,
-          header: false,
-          noneSelectedText: "...",
-          classes: "editor-multiselect",
-          selectedList: 1,
-          close: function(event, ui) {
-            //args.grid.getEditorLock().commitCurrentEdit();
-          }
-        });
-    };
-
-    this.destroy = function() {
-        $select.multiselect("destroy");
-        $select.remove();
-    };
-
-    this.save = function () {
-      args.commitChanges();
-    };
-
-    this.cancel = function () {
-      this.setDirectValue(defaultValue);
-      args.cancelChanges();
-    };
-
-    this.hide = function () {
-      $select.hide();
-    };
-
-    this.show = function () {
-      $select.show();
-    };
-
-    this.position = function (position) {
-      // nada 
-    };
-
-    this.focus = function() {
-        $select.focus();
-    };
-
-    this.setDirectValue = function (val) {
-        var key = getKeyFromKeyVal(opt, val);
-        key = opt[key].key;
-        defaultValue = key;
-        $select.val(key);
-        $select.multiselect("refresh");
-    };
-
-    this.loadValue = function (item) {
-      scope.setDirectValue(args.grid.getDataItemValueForColumn(item, args.column));
-      $select.select();
-    };
-
-    this.serializeValue = function() {
-        return $select.val();
-    };
-
-    this.applyValue = function(item, state) {
-      args.grid.setDataItemValueForColumn(item, args.column, state);
-    };
-
-    this.isValueChanged = function() {
-        return scope.serializeValue() != defaultValue;
-    };
-
-    this.validate = function() {
-        return {
-            valid: true,
-            msg: null
-        };
-    };
-
-    this.init();
-  }
-
   function YesNoSelectEditor(args) {
     var $select;
     var defaultValue;
@@ -1517,24 +1403,60 @@ function isValidModifier(v) {
 
 
   function SelectCellEditor(args) {
-    var $select, defaultValue,
-      scope = this;
+    var $select;
+    var defaultValue;
+    var scope = this;
+    var opt;
+
+    function getKeyFromKeyVal(opt, val) {
+      var i, v, index = 0;
+
+      for (i in opt) {
+        v = opt[i];
+        if (v.val === val) {
+          index = i;
+          break;
+        }
+      }
+      return index;
+    }
 
     this.init = function() {
-      var options = typeof args.column.options === 'function' ? args.column.options() : args.column.options;
-      if (options) {
-        var option_str = "";
-        for (var i in options) {
-          v = options[i];
-          option_str += "<OPTION value='" + ( v.key == null ? v.id : v.key ) + "'>" + (v.value == null ? v.label : v.value) + "</OPTION>";
+      var i;
+
+      defaultValue = null;
+      opt = (args.metadataColumn && args.metadataColumn.options) || args.column.options;
+      assert(opt);
+      opt = typeof opt === 'function' ? opt.call(args.column) : opt;
+      assert(opt);
+
+        option_str = [];
+        for (i in opt) {
+          v = opt[i];
+          option_str.push("<OPTION value='" + (v.key == null ? v.id : v.key) + "'>" + (v.value == null ? v.label : v.value) + "</OPTION>");
         }
-        $select = $("<SELECT tabIndex='0' class='editor-select'>" + option_str + "</SELECT>");
-        $select.appendTo(args.container);
-        $select.focus();
-      }
+        $select = $("<SELECT tabIndex='0' class='editor-select'>" + option_str.join('') + "</SELECT>")
+         .appendTo(args.container)
+         .focus()
+         .select();
+
+        // this expects the multiselect widget (http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/) to be loaded
+        $select.multiselect({
+          autoOpen: true,
+          minWidth: $(args.container).innerWidth() - 5,
+          multiple: false,
+          header: false,
+          noneSelectedText: "...",
+          classes: "editor-multiselect",
+          selectedList: 1,
+          close: function(event, ui) {
+            //args.grid.getEditorLock().commitCurrentEdit();
+          }
+        });
     };
 
     this.destroy = function() {
+      $select.multiselect("destroy");
       $select.remove();
     };
 
@@ -1563,25 +1485,29 @@ function isValidModifier(v) {
       $select.focus();
     };
 
-    this.loadValue = function(item) {
-      defaultValue = args.grid.getDataItemValueForColumn(item, args.column) || "";
-      $select.val(defaultValue);
+    this.setDirectValue = function (val) {
+        var key = getKeyFromKeyVal(opt, val);
+        key = opt[key].key;
+        defaultValue = key;
+        $select.val(key);
+        $select.multiselect("refresh");
     };
 
-    this.serializeValue = function() {
-      if (args.column.options) {
-        return $select.val();
-      } else {
-        return ($select.val() == "yes");
-      }
+    this.loadValue = function (item) {
+      scope.setDirectValue(args.grid.getDataItemValueForColumn(item, args.column));
+      $select.select();
     };
 
-    this.applyValue = function(item,state) {
+    this.serializeValue = function () {
+      return $select.val();
+    };
+
+    this.applyValue = function (item, state) {
       args.grid.setDataItemValueForColumn(item, args.column, state);
     };
 
     this.isValueChanged = function() {
-        return ($select.val() != defaultValue);
+      return scope.serializeValue() != defaultValue;
     };
 
     this.validate = function() {
